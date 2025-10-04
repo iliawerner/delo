1c05f1947da92951a58746740854af42c37c5f33
iliawerner <aidesignedutech@gmail.com>
2025-10-04 12:50:40 -0700

    Fix Vercel analytics import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react ;';
-import { Analytics } from '@vercel/analytics/next'
+import { useState } from 'react';
+import { Analytics } from '@vercel/analytics/react'
 import { Button } from '@/components/ui/button.jsx'
 import { Card, CardContent } from '@/components/ui/card.jsx'
 import { Input } from '@/components/ui/input.jsx'
